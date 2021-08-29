13584158b782c60db46ee62de54f29459656078c
Guilherme Barros <wingui3@gmail.com>
2021-08-29 08:06:49 -0300

    🚧 curso em andamento

---
+++ 03-Efeitos-no-DOM/02-modules-introcucao/js/modules/teste.js
@@ -0,0 +1,7 @@
+export function teste1() {
+  console.log('isso é teste');
+}
+
+export function teste2() {
+  console.log('isso é teste 2');
+}
---
+++ 03-Efeitos-no-DOM/02-modules-introcucao/js/script.js
@@ -0,0 +1,15 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabNav.js';
+// import { teste1, teste2 } from './modules/teste.js';
+import * as teste from './modules/teste.js';
+teste.teste1();
+teste.teste2();
+
+initAnimacaoScroll();
+initScrollSuave();
+initAccordion();
+initTabNav();
+// teste1();
+// teste2();
