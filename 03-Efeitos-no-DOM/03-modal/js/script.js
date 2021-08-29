85f582bd17ec71ccd447abd259f75dccaf9f180d
Guilherme Barros <wingui3@gmail.com>
2021-08-29 15:15:15 -0300

    🚧 curso em andamento

--- 03-Efeitos-no-DOM/03-modal/js/script.js
+++ 03-Efeitos-no-DOM/03-modal/js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabNav.js';
+import initModal from './modules/modal.js';
 
 initAnimacaoScroll();
 initScrollSuave();
 initAccordion();
 initTabNav();
+initModal();
